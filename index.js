8f72364debdd9324b6510fe091da1541707774b2
Arthur Song <arth3rs0ng@gmail.com>
2019-03-28 05:00:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function deleteFromObjectByKey (object, key){
   return newObj
 }
 
-function destructivelylDeleteFromObjectByKey(object, key){
+function destructivelyDeleteFromObjectByKey(object, key){
   delete object.key
   return object
 }
